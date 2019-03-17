00ddf4ea899ac761d313639500081e4135eda234
Pete Hunter <pete.hunter@piksel.com>
2019-03-17 14:58:13 +0000

    Changed star render step time.

--- Data/Game/Solar_System/Stars/renderers/star.lua
+++ Data/Game/Solar_System/Stars/renderers/star.lua
@@ -6,9 +6,8 @@ function star.draw(x, y, sc, ent)
   local hw = ent.body.w * .5
   local hh = ent.body.h * .5
   local star_scale = 10
-  ent.star.step = ent.star.step + ent.star.step_inc
   love.graphics.setColor(1, 1, 1, 1)
-  engine.shaders.star:send("iGlobalTime", ent.star.step)
+  engine.shaders.star:send("iGlobalTime", engine.star_step)
   love.graphics.setShader(engine.shaders.star)
   love.graphics.draw(engine.assets.star, x, y, r, sc * star_scale, sc * star_scale, hw, hh)
   love.graphics.setShader()
